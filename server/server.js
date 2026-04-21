69a43ae3d0c0c25ffb3ed015b384b932270c110e
Arjun Ghuge <arjunghuge644@gmail.com>
2026-04-22 01:23:02 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
 }
